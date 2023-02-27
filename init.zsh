19944e8040bef043d5cbc38bbaf4fff73706e683
John Sockwell <johnsockwell@users.noreply.github.com>
2023-02-27 15:11:41 -0500

    feat: add 'config' remote if missing

--- init.zsh
+++ init.zsh
@@ -5,11 +5,13 @@
 
 # Initialization Parameters
 declare -r config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}"
+declare -r config_remote="git@github.com:johnsockwell/config.git"
 
 # Initialize Config Repo
 function init_repo () {
   mkdir -p "${config_dir}"
   git -C "${config_dir}" init --quiet
+  git -C "${config_dir}" remote set-url origin "${config_remote}"
 }
 
 # Process Command
