82854ce3874b80423b290caa8b7c3b107fe35876
John Sockwell <johnsockwell@users.noreply.github.com>
2023-02-27 14:12:48 -0500

    feat: create 'config' directory if missing

--- init.zsh
+++ init.zsh
@@ -3,9 +3,21 @@
 # Initiaize a ZSH environment.
 #
 
+# Initialization Parameters
+declare -r config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}"
+
+# Initialize Config Repo
+function init_repo () {
+  mkdir -p "${config_dir}"
+}
+
 # Process Command
 case ${1} in
+  repo)
+    init_repo
+    ;;
   *)
     # default
+    init_repo
     ;;
 esac
